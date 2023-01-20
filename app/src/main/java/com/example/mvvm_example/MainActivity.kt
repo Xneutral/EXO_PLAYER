db0f3f8b2e09f908ba1ae7328d0c414f1fc86925
unknown <rashidsanghera7@gmail.com>
2023-01-20 12:55:08 +0500

    commit

---
+++ app/src/main/java/com/example/mvvm_example/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.mvvm_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvm_example.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity()
+{
+    private val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+    lateinit var viewModel: MainViewModel
+    private val retrofitService = RetrofitService.getInstance()
+    val adapter = MainAdapter()
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this,
+            MyViewModelFactory(MainRepository(retrofitService))
+        )
+            .get(MainViewModel::class.java)
+        val viewModel = ViewModelProvider(this, MyViewModelFactory(MainRepository(RetrofitService.retrofitService!!))).get(MainViewModel::class.java)
+
+        viewModel.getAllMovies()
+        binding.recyclerview.adapter = adapter
+        viewModel.movieList.observe(this, Observer {
+            Log.d(TAG, "onCreate: $it")
+            adapter.setMovieList(it)
+        })
+        viewModel.errorMessage.observe(this, Observer {
+        })
+        viewModel.getAllMovies()
+
+
+    }
+}
